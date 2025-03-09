efcc5232b8b0b86e1cd43e95929c916cf076dc6a
satish chandra <imsachandra@gmail.com>
2025-03-10 03:14:59 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to SATISH's World !
+        </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> And Let's <b>C</b>onnect!"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>HeY EverYoNe! My Name is Satish Chandra </p>
+          <p className="text-gray-500">
+            I am a Full Stack Developer and a Tech Enthusiast. I love to build
+            things that make a difference. I am a passionate learner and always
+            eager to learn new technologies.
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
